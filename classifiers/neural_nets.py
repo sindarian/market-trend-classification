5c7232ad4e553b1e3bb394038da9af7a96564be5
sindarian <sindarian@users.noreply.github.com>
2025-03-11 14:50:34 -0400

    Creating one train_df, onehot_df, and test_df for all networks to evaluate on

--- classifiers/neural_nets.py
+++ classifiers/neural_nets.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, Flatten, Conv1D
+from keras.layers import Dense, LSTM, Flatten, Conv1D, Input
 
 def build_mlp_clf(model_config_dict):
     # extract stuff from config
@@ -20,6 +20,27 @@ def build_mlp_clf(model_config_dict):
     metrics = model_config_dict['metrics']
 
     model.compile(optimizer='adam', loss=loss_str, metrics=metrics)
+
+    return model
+
+def build_mlp_clf_2(config_in_shape, config_out_shape, final_activ, config_loss_str, config_metrics, optimizer):
+    # extract stuff from config
+    in_shape = config_in_shape
+    out_shape = config_out_shape
+    activ = final_activ
+
+    # build model
+    model = Sequential(name="MLP")
+    model.add(Input(shape=(in_shape,)))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(out_shape, activation=activ))
+
+    # compile model
+    loss_str = config_loss_str
+    metrics = config_metrics
+
+    model.compile(optimizer=optimizer, loss=loss_str, metrics=metrics)
 
     return model
 
@@ -45,6 +66,27 @@ def build_lstm_clf(model_config_dict):
 
     return model
 
+def build_lstm_clf_2(config_in_shape, config_out_shape, final_activ, config_loss_str, config_metrics, optimizer):
+    # extract stuff from config
+    in_shape = config_in_shape
+    out_shape = config_out_shape
+    activ = final_activ
+
+    # build model
+    model = Sequential(name="LSTM")
+    model.add(Input(shape=(in_shape,1)))
+    model.add(LSTM(units=50, activation='relu', dropout=.10, return_sequences=True))
+    model.add(Flatten())
+    model.add(Dense(out_shape, activation=activ))
+
+    # compile model
+    loss_str = config_loss_str
+    metrics = config_metrics
+
+    model.compile(optimizer=optimizer, loss=loss_str, metrics=metrics)
+
+    return model
+
 def build_cnn_clf(model_config_dict):
     # extract stuff from config
     in_shape = model_config_dict['in_shape']
@@ -64,6 +106,27 @@ def build_cnn_clf(model_config_dict):
     metrics = model_config_dict['metrics']
 
     model.compile(optimizer='adam', loss=loss_str, metrics=metrics)
+
+    return model
+
+def build_cnn_clf_2(config_in_shape, config_out_shape, final_activ, config_loss_str, config_metrics, optimizer):
+    # extract stuff from config
+    in_shape = config_in_shape
+    out_shape = config_out_shape
+    activ = final_activ
+
+    # build model
+    model = Sequential(name="CNN")
+    model.add(Input(shape=(in_shape,1)))
+    model.add(Conv1D(filters=100, kernel_size=5, activation='sigmoid'))
+    model.add(Flatten())
+    model.add(Dense(out_shape, activation=activ))
+
+    # compile model
+    loss_str = config_loss_str
+    metrics = config_metrics
+
+    model.compile(optimizer=optimizer, loss=loss_str, metrics=metrics)
 
     return model
 
--- nn_train_driver.py
+++ nn_train_driver.py
@@ -5,6 +5,11 @@ import classifiers.neural_nets as nn
 import classifiers.neural_net_feature_space as nnfs
 import classifiers.labeller as labeller
 
+from sklearn.model_selection import GridSearchCV
+from scikeras.wrappers import KerasClassifier
+
+from pprint import pprint
+
 
 def train_mlp(data_df, label_df=None, config=None):
     # set the config if needed
@@ -44,9 +49,64 @@ def train_mlp(data_df, label_df=None, config=None):
     feature_cols = list(train_df.columns)
     feature_cols.remove('EpochTime')
     feature_cols.remove('Label')
+    
     model.fit(train_df[feature_cols].values, label_onehot, epochs=config['epochs'])
 
     return model, test_df
+
+def train_mlp_grid_search(train_df, label_onehot, config):
+    # # set the config if needed
+    # if config is None:
+    #     config = default_training_config()
+
+    # # compute label space if needed
+    # if label_df is None:
+    #     label_df = labeller.driver(data_df,
+    #                                n_components=config['n_fft_components'],
+    #                                signal_column=config['signal_data_column'])
+
+    # # compute feature space
+    # feature_space_df = nnfs.compute_feature_space_df(data_df,
+    #                                                  config['periods'],
+    #                                                  data_col=config['signal_data_column'])
+
+    # # time align
+    # label_df = label_df.loc[np.in1d(label_df.EpochTime.values, feature_space_df.EpochTime.values)]
+
+    # # merge the two DF so that the labels and feature values appear alongside each other
+    # feature_space_df = pd.merge(feature_space_df, label_df[['EpochTime', 'Label']], how='left')
+
+    # # train/test split
+    # split_factor = .8
+    # train_df, test_df = (feature_space_df.iloc[:int(feature_space_df.shape[0] * split_factor)],
+    #                      feature_space_df.iloc[int(feature_space_df.shape[0] * split_factor):])
+
+    # # shuffle rows of train df, onehot encode
+    # train_df = train_df.sample(frac=1).reset_index(drop=True)
+    # label_onehot = convert_onehot(train_df.Label.values)
+    
+    feature_cols = list(train_df.columns)
+    feature_cols.remove('EpochTime')
+    feature_cols.remove('Label')
+
+    model = KerasClassifier(model=nn.build_mlp_clf_2, verbose=0)
+
+    # define the grid search parameters
+    optimizers = ['SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam']
+    param_grid = dict(model__config_in_shape=[config['in_shape']],
+                      model__config_out_shape=[config['out_shape']],
+                      model__final_activ=[config['activation']],
+                      model__config_loss_str=[config['loss']],
+                      model__config_metrics=[config['metrics']],
+                      model__optimizer=optimizers)
+    grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=3)
+    grid_result = grid.fit(train_df[feature_cols].values, label_onehot)
+
+    pprint(f'Best MLP found with: {grid_result.best_params_}')
+
+    model = grid_result.best_estimator_
+    
+    return model #, test_df
 
 def train_lstm(data_df, label_df=None, config=None):
     # set the config if needed
@@ -86,11 +146,67 @@ def train_lstm(data_df, label_df=None, config=None):
     feature_cols = list(train_df.columns)
     feature_cols.remove('EpochTime')
     feature_cols.remove('Label')
-    print(train_df[feature_cols].values.shape)
-    print(label_onehot.shape)
+
     model.fit(train_df[feature_cols].values, label_onehot, epochs=config['epochs'])
 
     return model, test_df
+
+# def train_lstm_grid_search(data_df, label_df=None, config=None):
+def train_lstm_grid_search(train_df, label_onehot, config):
+    # # set the config if needed
+    # if config is None:
+    #     config = default_training_config()
+
+    # # compute label space if needed
+    # if label_df is None:
+    #     label_df = labeller.driver(data_df,
+    #                                n_components=config['n_fft_components'],
+    #                                signal_column=config['signal_data_column'])
+
+    # # compute feature space
+    # feature_space_df = nnfs.compute_feature_space_df(data_df,
+    #                                                  config['periods'],
+    #                                                  data_col=config['signal_data_column'])
+
+    # # time align
+    # label_df = label_df.loc[np.in1d(label_df.EpochTime.values, feature_space_df.EpochTime.values)]
+
+    # # merge the two DF so that the labels and feature values appear alongside each other
+    # feature_space_df = pd.merge(feature_space_df, label_df[['EpochTime', 'Label']], how='left')
+
+    # # train/test split
+    # split_factor = .8
+    # train_df, test_df = (feature_space_df.iloc[:int(feature_space_df.shape[0] * split_factor)],
+    #                      feature_space_df.iloc[int(feature_space_df.shape[0] * split_factor):])
+
+    # # shuffle rows of train df, onehot encode
+    # train_df = train_df.sample(frac=1).reset_index(drop=True)
+    # label_onehot = convert_onehot(train_df.Label.values)
+
+    # train model
+    feature_cols = list(train_df.columns)
+    feature_cols.remove('EpochTime')
+    feature_cols.remove('Label')
+
+    model = KerasClassifier(model=nn.build_lstm_clf_2, verbose=0)
+
+    # define the grid search parameters
+    optimizers = ['SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam']
+    param_grid = dict(model__config_in_shape=[config['in_shape']],
+                      model__config_out_shape=[config['out_shape']],
+                      model__final_activ=[config['activation']],
+                      model__config_loss_str=[config['loss']],
+                      model__config_metrics=[config['metrics']],
+                      model__optimizer=optimizers)
+    param_grid['epochs'] = [3]
+    grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=3)
+    grid_result = grid.fit(train_df[feature_cols].values, label_onehot)
+
+    pprint(f'Best LSTM found with: {grid_result.best_params_}')
+
+    model = grid_result.best_estimator_
+
+    return model#, test_df
 
 def train_cnn(data_df, label_df=None, config=None):
     # set the config if needed
@@ -134,6 +250,61 @@ def train_cnn(data_df, label_df=None, config=None):
 
     return model, test_df
 
+def train_cnn_grid_search(train_df, label_onehot, config):
+    # # set the config if needed
+    # if config is None:
+    #     config = default_training_config()
+
+    # # compute label space if needed
+    # if label_df is None:
+    #     label_df = labeller.driver(data_df,
+    #                                n_components=config['n_fft_components'],
+    #                                signal_column=config['signal_data_column'])
+
+    # # compute feature space
+    # feature_space_df = nnfs.compute_feature_space_df(data_df,
+    #                                                  config['periods'],
+    #                                                  data_col=config['signal_data_column'])
+
+    # # time align
+    # label_df = label_df.loc[np.in1d(label_df.EpochTime.values, feature_space_df.EpochTime.values)]
+
+    # # merge the two DF so that the labels and feature values appear alongside each other
+    # feature_space_df = pd.merge(feature_space_df, label_df[['EpochTime', 'Label']], how='left')
+
+    # # train/test split
+    # split_factor = .8
+    # train_df, test_df = (feature_space_df.iloc[:int(feature_space_df.shape[0] * split_factor)],
+    #                      feature_space_df.iloc[int(feature_space_df.shape[0] * split_factor):])
+
+    # # shuffle rows of train df, onehot encode
+    # train_df = train_df.sample(frac=1).reset_index(drop=True)
+    # label_onehot = convert_onehot(train_df.Label.values)
+
+    # train model
+    feature_cols = list(train_df.columns)
+    feature_cols.remove('EpochTime')
+    feature_cols.remove('Label')
+
+    model = KerasClassifier(model=nn.build_cnn_clf_2, verbose=0)
+
+    # define the grid search parameters
+    optimizers = ['SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam']
+    param_grid = dict(model__config_in_shape=[config['in_shape']],
+                      model__config_out_shape=[config['out_shape']],
+                      model__final_activ=[config['activation']],
+                      model__config_loss_str=[config['loss']],
+                      model__config_metrics=[config['metrics']],
+                      model__optimizer=optimizers)
+    grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=3)
+    grid_result = grid.fit(train_df[feature_cols].values, label_onehot)
+
+    pprint(f'Best CNN found with: {grid_result.best_params_}')
+
+    model = grid_result.best_estimator_
+
+    return model#, test_df
+
 
 def default_training_config():
     periods = np.arange(2, 20, 2)
@@ -149,7 +320,6 @@ def default_training_config():
 
     return config
 
-
 def convert_onehot(arr):
     return np.eye(2)[arr.astype(int)]
 
@@ -162,6 +332,39 @@ def inverse_onehot(onehot_arr):
         arr[insert_idx] = c_idx
 
     return arr
+
+def split_data(data_df, label_df=None, config=None):
+    # set the config if needed
+    if config is None:
+        config = default_training_config()
+
+    # compute label space if needed
+    if label_df is None:
+        label_df = labeller.driver(data_df,
+                                   n_components=config['n_fft_components'],
+                                   signal_column=config['signal_data_column'])
+
+    # compute feature space
+    feature_space_df = nnfs.compute_feature_space_df(data_df,
+                                                     config['periods'],
+                                                     data_col=config['signal_data_column'])
+
+    # time align
+    label_df = label_df.loc[np.in1d(label_df.EpochTime.values, feature_space_df.EpochTime.values)]
+
+    # merge the two DF so that the labels and feature values appear alongside each other
+    feature_space_df = pd.merge(feature_space_df, label_df[['EpochTime', 'Label']], how='left')
+
+    # train/test split
+    split_factor = .8
+    train_df, test_df = (feature_space_df.iloc[:int(feature_space_df.shape[0] * split_factor)],
+                         feature_space_df.iloc[int(feature_space_df.shape[0] * split_factor):])
+
+    # shuffle rows of train df, onehot encode
+    train_df = train_df.sample(frac=1).reset_index(drop=True)
+    label_onehot = convert_onehot(train_df.Label.values)
+
+    return train_df, label_onehot, test_df
 
 
 if __name__ == '__main__':
